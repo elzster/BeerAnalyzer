27d0981b5438f8bb7f00ddae208b8b82efcb4389
elzster <hakim.elie1@gmail.com>
2020-02-06 23:19:21 -0500

    Files Added in

--- beer/static/js/tableau1.js
+++ beer/static/js/tableau1.js
@@ -1,14 +1,43 @@
-function initializeViz() {
-  // JS object that points at empty div in the html
+// function initializeViz() {
+//   // JS object that points at empty div in the html
+//   var placeholderDiv = document.getElementById("tableauViz");
+//   // URL of the viz to be embedded
+//   var url = "https://public.tableau.com/profile/elie2805#!/vizhome/BeerBooklet/AVGTotalReviews2;"
+//   // An object that contains options specifying how to embed the viz
+//   var options = {
+//     width: '600px',
+//     height: '600px',
+//     hideTabs: true,
+//     hideToolbar: true,
+//   };
+//   viz = new tableau.Viz(placeholderDiv, url, options);
+// }
+
+$( document ).ready(function() {
+  var viz, workbook, activeSheet;
   var placeholderDiv = document.getElementById("tableauViz");
-  // URL of the viz to be embedded
-  var url = "http://public.tableau.com/views/WorldIndicators/GDPpercapita";
-  // An object that contains options specifying how to embed the viz
+ 
+  var url = "https://public.tableau.com/views/BeerBooklet/AVGTotalReviews2?:display_count=y&:origin=viz_share_link";
   var options = {
-    width: '600px',
-    height: '600px',
+    width: "100%",
+    height: "600px",
     hideTabs: true,
     hideToolbar: true,
+    onFirstInteractive: function () {
+      workbook = viz.getWorkbook();
+      activeSheet = workbook.getActiveSheet();
+    }
   };
-  viz = new tableau.Viz(placeholderDiv, url, options);
-}
+  viz = new tableau.Viz(placeholderDiv, url, options);  
+ 
+  $(".apply_RegionFilter").click(function() {
+    activeSheet.applyFilterAsync(
+      "Region",
+      $(this).text(),
+      tableau.FilterUpdateType.REPLACE);
+  });
+ 
+  $(".selectAll_RegionFilter").click(function() {
+    activeSheet.clearFilterAsync("Region");
+  });
+});
