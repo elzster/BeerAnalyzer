47bee49fa71a0ac6622b62629363ff2f2c35de0f
elzster <hakim.elie1@gmail.com>
2020-02-09 17:51:03 -0500

    JS file, Requirements.txt

--- beer/static/js/tableau1.js
+++ beer/static/js/tableau1.js
@@ -1,38 +1,13 @@
-// function initializeViz() {
-//   // JS object that points at empty div in the html
-//   var placeholderDiv = document.getElementById("tableauViz");
-//   // URL of the viz to be embedded
-//   var url = "https://public.tableau.com/profile/elie2805#!/vizhome/BeerBooklet/AVGTotalReviews2;"
-//   // An object that contains options specifying how to embed the viz
-//   var options = {
-//     width: '600px',
-//     height: '600px',
-//     hideTabs: true,
-//     hideToolbar: true,
-//   };
-//   viz = new tableau.Viz(placeholderDiv, url, options);
-// }
 
 $( document ).ready(function() {
   var viz, workbook, activeSheet;
   var placeholderDiv = document.getElementById("tableauViz");
  
-<<<<<<< HEAD
-  var url = "https://public.tableau.com/profile/jean.p1562#!/vizhome/BeerAnalysisProject2/ExploratoryAnalysis";
-<<<<<<< HEAD
-
-=======
->>>>>>> elie
-  var options = {
-    width: "100%",
-    height: "600px",
-=======
   var url = "https://public.tableau.com/views/BeerAnalysis_15812163851300/ExploratoryAnalysis?:display_count=y&publish=yes&:origin=viz_share_link";
   
   var options = {
     width: "10000px",
     height: "1000px",
->>>>>>> elie
     hideTabs: true,
     hideToolbar: true,
     onFirstInteractive: function () {
