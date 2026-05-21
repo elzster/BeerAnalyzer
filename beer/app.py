c318d5c70678a2514b500c71b97cecd046af443e
Elie <hakim.elie1@gmail.com>
2026-05-21 15:12:01 -0400

    Update app.py
    Fixed broken App.py

--- beer/app.py
+++ beer/app.py
@@ -1,147 +1,90 @@
-# import necessary libraries
-from flask import (Flask,render_template,jsonify,request,redirect,url_for)
+from pathlib import Path
+
 import pandas as pd
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-import sqlite3
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 
-#Machine Learning Bits Imported into Flask Server
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from .mlscript import similarity_model, get_title_from_index, get_index_from_title,print_statement, get_abv_from_index, get_beerstyle_from_index, get_brewery_from_index 
+from .mlscript import recommender
 
-#################################################
-# Flask Setup
-#################################################
+
+BASE_DIR = Path(__file__).resolve().parent
+CSV_PATH = BASE_DIR / "static" / "df500.csv"
+
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config["SECRET_KEY"] = "7d441f27d441f27567d441f2b6176a"
 
-#################################################
-# Database Setup
-#################################################
-import os 
-cd = os.getcwd()
 
-#Database Variables
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/brew.sqlite"
-db = SQLAlchemy(app)
-from .models import beerdata
-Base = automap_base()
-Base.prepare(db.engine, reflect=True)
+def get_beer_list():
+    beerlist = pd.read_csv(CSV_PATH)
 
-###############################################
-#########Html Routes for Web Server ###########
-###############################################
-####Landing Page####
+    if "beer_name" not in beerlist.columns:
+        raise ValueError("df500.csv must contain a 'beer_name' column.")
+
+    return beerlist["beer_name"]
+
+
+def render_recommendations(beer_name="Wachusett Larry"):
+    beerlist1 = get_beer_list()
+
+    try:
+        case_list = recommender.recommend(beer_name, limit=5)
+        error = None
+    except ValueError:
+        case_list = []
+        error = f"Beer '{beer_name}' was not found. Please choose a beer from the list."
+
+    result = {item["beer"]: item["similarity"] for item in case_list}
+    result2 = {item["beer"]: item["abv"] for item in case_list}
+
+    return render_template(
+        "finaltable.html",
+        result=result,
+        result2=result2,
+        beer_user_likes=beer_name,
+        beerlist1=beerlist1,
+        case_list=case_list,
+        error=error,
+    )
+
+
 @app.route("/")
 def index():
-
     return render_template("index.html")
+
 
 @app.route("/scope/")
 def scope():
     return render_template("scope.html")
 
+
 @app.route("/analyst/")
 def analyst():
     return render_template("analyst.html")
 
+
 @app.route("/analysis/")
 def analysis():
     return render_template("analysis.html")
+
 
 @app.route("/sentiment/")
 def sentiment():
     return render_template("sentiment.html")
 
 
-#Default Route that is Loaded Up into Script "Waschusett Larry"
-@app.route('/beer/', defaults={'beer': 'Wachusett Larry'})
-@app.route('/beer/<beer>')
-def beer_input(beer):
-    #loading up variables to use for MLscript
-    beerlist = pd.read_csv(cd+"/beer/static/df500.csv")
-    beerlist1 = beerlist['beer_name']
-    
-    #load up CSV for Matrix.
-    df_5000 = pd.read_csv(cd+"/beer/static/df500.csv")
-    count_matrix = CountVectorizer().fit_transform(df_5000["combined_features"])
-    cosine_sim = cosine_similarity(count_matrix)
-    cosine_sim.shape
-    beer_user_likes = (beer)
-    beer_index = get_index_from_title(beer_user_likes)
-    similar_beers = list( enumerate(cosine_sim[beer_index]) )
-    sorted_similar_beers = sorted(similar_beers,key = lambda x:x[1], reverse = True)[1:]
-    i=0
-    beer_dict = []
-    sim_score = []
-    abv_score = []
-    beer = {}
-    beer['beer'] ={}
-    beer['similarity']={}
-    beer['abv']={}
-    beer['brewery']={}
-    case_list=[]
-    
-    
-    for i in range(len(sorted_similar_beers)):
-        beer_dict.append(get_title_from_index(sorted_similar_beers[i][0]))
-        sim_score.append(sorted_similar_beers[i][1])
-        abv_score.append(get_abv_from_index(sorted_similar_beers[i][0]))
-        case = {'beer': get_title_from_index(sorted_similar_beers[i][0]), 'similarity': (sorted_similar_beers[i][1]).round(2), 'abv':(get_abv_from_index(sorted_similar_beers[i][0])), 'style':(get_beerstyle_from_index((sorted_similar_beers[i][0]))), 'brewery':(get_brewery_from_index((sorted_similar_beers[i][0]))) }
-        case_list.append(case)
-        if i>=4:
-            break
-    result = (dict(zip(beer_dict, sim_score)))
-    result2 = dict(zip(beer_dict, abv_score))
+@app.route("/beer/", methods=["GET"])
+def beer_default():
+    return render_recommendations("Wachusett Larry")
 
-    return render_template("finaltable.html", result=result, result2=result2, beer_user_likes=beer_user_likes, beerlist1=beerlist1, case_list=case_list)
 
-#Testing (Route is Hard Coded)
-@app.route('/beer/', methods=["POST"])
-def beer_input1():
+@app.route("/beer/", methods=["POST"])
+def beer_input():
+    beer_name = request.form.get("mybeer", "Wachusett Larry").strip()
 
-    beer = request.form['mybeer']
-    
-    beerlist = pd.read_csv(cd+"/beer/static/df500.csv")
-    beerlist1 = beerlist['beer_name']
-    
+    if not beer_name:
+        beer_name = "Wachusett Larry"
 
-    df_5000 = pd.read_csv(cd+"/beer/static/df500.csv")
-    count_matrix = CountVectorizer().fit_transform(df_5000["combined_features"])
-    cosine_sim = cosine_similarity(count_matrix)
-    cosine_sim.shape
-    beer_user_likes = (beer)
-    beer_index = get_index_from_title(beer_user_likes)
-    similar_beers = list( enumerate(cosine_sim[beer_index]) )
-    sorted_similar_beers = sorted(similar_beers,key = lambda x:x[1], reverse = True)[1:]
-    i=0
-    beer_dict = []
-    sim_score = []
-    abv_score = []
-    beer = {}
-    beer['beer'] ={}
-    beer['similarity']={}
-    beer['abv']={}
-    case_list=[]
+    return render_recommendations(beer_name)
 
-    for i in range(len(sorted_similar_beers)):
-        beer_dict.append(get_title_from_index(sorted_similar_beers[i][0]))
-        sim_score.append(sorted_similar_beers[i][1])
-        abv_score.append(get_abv_from_index(sorted_similar_beers[i][0]))
-        case = {'beer': get_title_from_index(sorted_similar_beers[i][0]), 'similarity': (sorted_similar_beers[i][1]).round(2), 'abv':(get_abv_from_index(sorted_similar_beers[i][0])), 'style':(get_beerstyle_from_index((sorted_similar_beers[i][0]))), 'brewery':(get_brewery_from_index((sorted_similar_beers[i][0]))) }
-        case_list.append(case)
-        if i>=4:
-            break
-    result = dict(zip(beer_dict, sim_score))
-    result2 = dict(zip(beer_dict, abv_score))
-
-    return render_template("finaltable.html", result=result, result2=result2, beer_user_likes=beer_user_likes, beerlist1=beerlist1, case_list=case_list)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
